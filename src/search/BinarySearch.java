1dcbea707b52aba0a8de54c44898671ebf94e27c
donRumata <donrumata03@gmail.com>
2022-02-15 12:56:42 +0300

    Add `main`'s correctness proof

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -219,28 +219,44 @@ public class BinarySearch {
      * @param args Such array that
      * — `Integer.parseInt(args[0])` executes correctly, denote it as `x`.
      * — Integer.parseInt can parse all items from `args[1:]` and collected parse result is denoted as array `a`
-     * Denote `a.size()` as `len`
-     * Prints minimal `i` \in [0, len) such that arg[i]
+     * Prints minimal `i` \in [0, a.length) such that a[i] <= x if any; else — prints `a.length`
      */
     public static void main(String[] args) {
         int x = Integer.parseInt(args[0]);
+        // — `x` executes correctly, denote it as `x`.
+        // — Integer.parseInt can parse all items from `args[1:]` and collected parse result is denoted as array `a`
 
         int[] a = new int[args.length - 1];
-        for (int i = 0; i < args.length - 1; i++) {
+        int i = 0;
+        // I: values from args[1..i + 1) are parsed into a[0..i) && i <= args.length - 1
+        while (i < args.length - 1) {
             a[i] = Integer.parseInt(args[i + 1]);
+            // values from args[1..i + 2) are parsed into a[0..i + 1) && i <= args.length - 1
+            i++;
+            // values from args[1..i + 1) are parsed into a[0..i) (=> invariant) && i <= args.length - 1
         }
+        // i == args.length - 1 => a[0..array.length) array `a` contains parsed args[1:]
+        // Prints minimal `i` \in [0, a.length) such that a[i] <= x if any; else — prints `a.length`
 
+        // a is non-str. mon. inc. => correct calls
         var iterativeSearched = arrayBinarySearch(a, x, BinarySearch::discreteIterativeBinarySearch);
         var recursiveSearched = arrayBinarySearch(a, x, BinarySearch::discreteRecursiveBinarySearch);
+        // both `iterativeSearched` and `recursiveSearched` ==
+        //  {
+        //      if there exists minimal `i` such that `array[i] <= value`
+        //      * — Some(i)
+        //      * — Otherwise, None
+        //  }
 
         if (!iterativeSearched.equals(recursiveSearched)) {
             throw new AssertionError("Iterative and recursive searches gave different results");
         }
 
         if (iterativeSearched.isPresent()) {
+            // there exists minimal `i` such that `array[i] <= value`
             System.out.println(iterativeSearched.get());
         } else {
-//            System.out.println("Fuck you!");
+            // there don't exist minimal `i` such that `array[i] <= value`
             System.out.println(a.length);
         }
     }
