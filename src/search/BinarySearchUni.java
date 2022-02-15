02d55a4c47ea215f3bb408e32d6244fa9504bee1
donRumata03 <donrumata03@gmail.com>
2022-02-15 16:36:26 +0300

    Add BinarySearchUni proof

--- src/search/BinarySearchUni.java
+++ src/search/BinarySearchUni.java
@@ -11,12 +11,20 @@ public class BinarySearchUni {
      * @return minimal size of such first array
      */
     static int minimalPrefixBeforeIncreasing(int[] array, DiscreteBinarySearchEngine engine) {
-
+        // predicate `(Integer index) -> index == array.length - 1 || array[index] < array[index + 1]`
+        // checks whether there are positions in `[0, index]` that can be the start of second array
+        // (
+        //  via extendedThresholdFunction indexes < 0 and >= array.length
+        //  are considered to have false and true correspondingly
+        // )
+        // proof: if index == array.length - 1, than by contract it's true
+        // otherwise it exists in somewhere in array but can't be after index => it's in [0, index]
+        // we get maximal index for which
         var searched = engine.search(
             (Integer index) -> index == array.length - 1 || array[index] < array[index + 1],
             -1, array.length
             );
-        // searched == minimal position from which second array can start
+        // searched.leftmostTrue == minimal position from which second array can start
 
         return searched.getLeftmostTrue();
     }
@@ -26,25 +34,20 @@ public class BinarySearchUni {
      * — Integer.parseInt can parse all items from `args` and collected parse result is denoted as array `a`
      * - `array` can be produced by concatenation of strictly decreasing and strictly increasing array
      * Prints minimal possible length of first such array
-     * <==> Prints minimal `i` \in [0, a.length) such that a[i] <= x if any; else — prints `a.length`
      */
     public static void main(String[] args) {
         int[] a = Arrays.stream(args).mapToInt(Integer::parseInt).toArray();
+        // => `a` is array from contract
 
         var iterativeSearched = minimalPrefixBeforeIncreasing(a, DiscreteIterativeBinarySearch::discreteIterativeBinarySearch);
+        var recursiveSearched = minimalPrefixBeforeIncreasing(a, DiscreteRecursiveBinarySearch::discreteRecursiveBinarySearch);
+        // They both by post-condition are minimal possible lengths of first such array
+
+        if (iterativeSearched != recursiveSearched) {
+            // Impossible
+            throw new AssertionError("Iterative and recursive searches gave different results");
+        }
 
         System.out.println(iterativeSearched);
-
-//        if (!iterativeSearched.equals(recursiveSearched)) {
-//            throw new AssertionError("Iterative and recursive searches gave different results");
-//        }
-
-//        if (iterativeSearched.isPresent()) {
-//            // there exists minimal `i` such that `array[i] <= value`
-//            System.out.println(iterativeSearched.get());
-//        } else {
-//            // there don't exist minimal `i` such that `array[i] <= value`
-//            System.out.println(a.length);
-//        }
     }
 }
