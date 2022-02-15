938dbbbfd96cc7d7e59cc57b10291ceab2b94d6c
donRumata <donrumata03@gmail.com>
2022-02-15 11:48:31 +0300

    Add recursive binary search correctness proof

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -78,15 +78,55 @@ public class BinarySearch {
      * Satisfies «Pre- and post- conditions for binary search» ↑↑↑
      */
     static DiscreteBinarySearchResult discreteRecursiveBinarySearch(Function<Integer, Boolean> thresholdFunction, int left, int right) {
+        // extendedThresholdFunction non-strictly monotonously increase at [left, right] &&
+        // extendedThresholdFunction(left) == false && extendedThresholdFunction(right) == true &&
+        // left < right
+
         if (left + 1 == right) {
-            return new DiscreteBinarySearchResult(left, right);
+            // left + 1 == right (=> can construct DiscreteBinarySearchResult)
+            // extendedThresholdFunction(l) == false && extendedThresholdFunction(r) == true
+            // (=> `extendedThresholdFunction(result.rightmostFalse) == false && extendedThresholdFunction(result.leftmostTrue) == true`)
+            return new DiscreteBinarySearchResult(left, right); // so, function's post-condition is satisfied
         }
 
+        // extendedThresholdFunction non-strictly monotonously increase at [left, right] &&
+        // extendedThresholdFunction(left) == false && extendedThresholdFunction(right) == true &&
+        // left + 1 < right &&
+        // (=>) (left + right) / 2 - left >= 1 &&
+        // (=>) right - (left + right) / 2 >= 1
         int m = (left + right) / 2;
+        // extendedThresholdFunction non-strictly monotonously increase at [left, right] &&
+        // extendedThresholdFunction(left) == false && extendedThresholdFunction(right) == true &&
+        // left + 1 < right &&
+        // m - left >= 1 &&
+        // right - m >= 1 &&
+        // (=>) m \in (left, right)
 
-        if (thresholdFunction.apply(m)) {
+        // (m - left >= 1 && right - m >= 1) => pre-condition for `thresholdFunction` is satisfied
+        boolean callResult = thresholdFunction.apply(m);
+        if (callResult) {
+            // extendedThresholdFunction non-strictly monotonously increase at [left, right] &&
+            // extendedThresholdFunction(left) == false && extendedThresholdFunction(right) == true &&
+            // left + 1 < right &&
+            // m \in (left, right)
+            // && extendedThresholdFunction(m) == true
+
+            // (=>) extendedThresholdFunction non-strictly monotonously increase at [left, m] &&
+            // (=>) extendedThresholdFunction(left) == false && extendedThresholdFunction(m) == true
+
+            // m - left + 1 <= right - left => recursion isn't infinite
             return discreteRecursiveBinarySearch(thresholdFunction, left, m);
         } else {
+            // extendedThresholdFunction non-strictly monotonously increase at [left, right] &&
+            // extendedThresholdFunction(left) == false && extendedThresholdFunction(right) == true &&
+            // left + 1 < right &&
+            // m \in (left, right)
+            // && extendedThresholdFunction(m) == false
+
+            // (=>) extendedThresholdFunction non-strictly monotonously increase at [m, right] &&
+            // (=>) extendedThresholdFunction(m) == false && extendedThresholdFunction(right) == true
+
+            // right - m + 1 <= right - left => recursion isn't infinite
             return discreteRecursiveBinarySearch(thresholdFunction, m, right);
         }
     }
