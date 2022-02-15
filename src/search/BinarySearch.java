1d021b89ae7a5fcb7e515e514f0b891befeabd54
donRumata <donrumata03@gmail.com>
2022-02-15 12:01:12 +0300

    Add iterative binary search correctness proof

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -56,21 +56,56 @@ public class BinarySearch {
         int l = left;
         int r = right;
 
-        // Invariant:
+        // Invariant (we get it from contract by denoting left as l, right as r):
         // extendedThresholdFunction non-strictly monotonously increase at [l, r] &&
         // extendedThresholdFunction(l) == false && extendedThresholdFunction(r) == true &&
         // l < r
 
+        // `r - l` decreases at each iteration at least by 1 => loop isn't infinite
         while (l + 1 != r) {
+            // extendedThresholdFunction non-strictly monotonously increase at [l, r] &&
+            // extendedThresholdFunction(l) == false && extendedThresholdFunction(r) == true &&
+            // l + 1 < r
             int m = (l + r) / 2;
+            // extendedThresholdFunction non-strictly monotonously increase at [l, r] &&
+            // extendedThresholdFunction(l) == false && extendedThresholdFunction(r) == true &&
+            // l + 1 < r &&
+            // m - l >= 1 &&
+            // r - m >= 1 &&
+            // (=>) m \in (left, right)
+
 
             if (thresholdFunction.apply(m)) {
+                // extendedThresholdFunction non-strictly monotonously increase at [l, r] &&
+                // extendedThresholdFunction(l) == false && extendedThresholdFunction(r) == true &&
+                // l + 1 < r &&
+                // m \in (left, right) &&
+                // extendedThresholdFunction(m) == true
                 r = m;
+                // extendedThresholdFunction non-strictly monotonously increase at [l, r] &&
+                // extendedThresholdFunction(l) == false && extendedThresholdFunction(r) == true &&
+                // l < r
             } else {
+                // extendedThresholdFunction non-strictly monotonously increase at [l, r] &&
+                // extendedThresholdFunction(l) == false && extendedThresholdFunction(r) == true &&
+                // l + 1 < r &&
+                // m \in (left, right) &&
+                // extendedThresholdFunction(m) == true
                 l = m;
+                // extendedThresholdFunction non-strictly monotonously increase at [l, r] &&
+                // extendedThresholdFunction(l) == false && extendedThresholdFunction(r) == true &&
+                // l < r
             }
+
+            // extendedThresholdFunction non-strictly monotonously increase at [l, r] &&
+            // extendedThresholdFunction(l) == false && extendedThresholdFunction(r) == true &&
+            // l < r
         }
 
+        // extendedThresholdFunction non-strictly monotonously increase at [l, r] &&
+        // extendedThresholdFunction(l) == false && extendedThresholdFunction(r) == true &&
+        // l < r &&
+        // r == l + 1 (=> constructing is correct)
         return new DiscreteBinarySearchResult(l, r);
     }
 
@@ -113,6 +148,7 @@ public class BinarySearch {
 
             // (=>) extendedThresholdFunction non-strictly monotonously increase at [left, m] &&
             // (=>) extendedThresholdFunction(left) == false && extendedThresholdFunction(m) == true
+            // (=>) m > left
 
             // m - left + 1 <= right - left => recursion isn't infinite
             return discreteRecursiveBinarySearch(thresholdFunction, left, m);
@@ -125,6 +161,7 @@ public class BinarySearch {
 
             // (=>) extendedThresholdFunction non-strictly monotonously increase at [m, right] &&
             // (=>) extendedThresholdFunction(m) == false && extendedThresholdFunction(right) == true
+            // (=>) right > m
 
             // right - m + 1 <= right - left => recursion isn't infinite
             return discreteRecursiveBinarySearch(thresholdFunction, m, right);
