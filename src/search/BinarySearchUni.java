9226bc89d4ac7e2f166ebb53ec8e39b82b7ad4a5
donRumata <donrumata03@gmail.com>
2022-02-21 23:22:38 +0300

    Add formal foundation for Uni proofs

--- src/search/BinarySearchUni.java
+++ src/search/BinarySearchUni.java
@@ -3,12 +3,47 @@ package search;
 import java.util.Arrays;
 import java.util.Optional;
 
+/**
+ * Some useful definitions are provided here…
+ *
+ * Borrow the definitions of strictly and non-strictly monotonous for real-valued, integer-*
+ * and boolean-* (consider false === \mathbb{0}; true === \mathbb{1}) functions.
+ * Abbreviations: SMI, NSMI, SMD, NSMD
+ *
+ * Integer intervals are denoted as [a, b)
+ *
+ * Denote function restriction to [a, b) as f|          or just `f|[a, b)`
+ *                                           |[a, b)
+ *
+ * We'll call function `f: [l, r) \subset \mathbb{Z} |-> (T: Ord)`
+ * Strictly Decreasing-Increasing Decayable (SDID) if there exists `m \in [l; r]` such that
+ * — \forall i < j in [l, m): f(i) > f(j)
+ * — \forall i < j in [m, r): f(i) < f(j)
+ *
+ * For a given `f` the set of such `i`s is denoted as splitters(f). So, `f` is SDID <=> there exists its splitters
+ *
+ * Array `a` is SDID if `acc: [0, a.length()) |-> decltype(a[0])` such that acc(i) = a[i] is SDID
+ *
+ * Note that being SDID for an array is the formalization of
+ * «being possible to be produced by concatenation of strictly decreasing and strictly increasing arrays (both may be empty)»
+ *
+ * Theorems for `SDID f: [l, r) |-> T`:
+ *
+ * Theorem S1: If f(m) < f(m + 1), then f|[m, r) is SMI.
+ *
+ * Theorem S2: Boolean function g: [l, r - 1) -> Bool, g(i) = (f(i) < f(i + 1)) is NSMI
+ *
+// * Theorem S3: and for some m \in [l, r): f(m) < f(m + 1), then f|           is SDID.
+// *                                                                                          | [l, m]
+ *
+ *
+ */
 public class BinarySearchUni {
 
     /**
      *
-     * @param array is uni-variate (can be produced by concatenation of strictly decreasing and strictly increasing arrays)
-     * @return minimal size of such first array
+     * @param array SDID
+     * @return the smallest splitter of `array`
      */
     static int minimalPrefixBeforeIncreasing(int[] array, DiscreteBinarySearchEngine engine) {
         // predicate `(Integer index) -> index == array.length - 1 || array[index] < array[index + 1]`
@@ -32,8 +67,8 @@ public class BinarySearchUni {
     /**
      * @param args Such array that
      * — Integer.parseInt can parse all items from `args` and collected parse result is denoted as array `a`
-     * - `array` can be produced by concatenation of strictly decreasing and strictly increasing array
-     * Prints minimal possible length of first such array
+     * — `a` is SDID
+     * Prints the smallest splitter of `a`. (a.k.a. minimal possible length of first such array)
      */
     public static void main(String[] args) {
         int[] a = Arrays.stream(args).mapToInt(Integer::parseInt).toArray();
