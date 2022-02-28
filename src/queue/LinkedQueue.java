3320f0883e7cc60ac76cb4c67a19c2ce39eb5eab
donRumata <donrumata03@gmail.com>
2022-02-28 22:22:47 +0300

    Introduce Linked Queue

---
+++ src/queue/LinkedQueue.java
@@ -0,0 +1,63 @@
+package queue;
+
+
+/**
+ * The only thing you need to know about this class is that it correctly implements Queue (checked by kgeorgiy)
+ */
+public class LinkedQueue implements Queue {
+
+    @Override
+    public void enqueue(Object element) {
+
+    }
+
+    @Override
+    public Object dequeue() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object element() {
+        return null;
+    }
+
+    @Override
+    public void push(Object element) {
+
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public Object remove() {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object element) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object element) {
+        return 0;
+    }
+}
