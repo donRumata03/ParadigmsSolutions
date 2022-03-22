9bef1dbf4bd96148df4b899de658f54269190737
donRumata03 <donrumata03@gmail.com>
2022-03-22 17:39:28 +0300

    Generalize Negate

--- src/expression/general/operations/Negate.java
+++ src/expression/general/operations/Negate.java
@@ -3,24 +3,20 @@ package expression.general.operations;
 import expression.general.ParenthesesTrackingExpression;
 import expression.general.UnaryOperation;
 import expression.general.UnaryOperatorTraits;
+import expression.general.arithmetics.ArithmeticEngine;
 import java.math.BigDecimal;
 
-public class Negate extends UnaryOperation {
-    static UnaryOperatorTraits OPERATOR_TRAITS = new UnaryOperatorTraits(
+public class Negate<T, Engine extends ArithmeticEngine<T>> extends UnaryOperation<T, Engine> {
+    static UnaryOperatorTraits OPERATOR_INFO = new UnaryOperatorTraits(
         "-"
     );
 
-    public Negate(ParenthesesTrackingExpression child) {
-        super(child, OPERATOR_TRAITS);
+    public Negate(ParenthesesTrackingExpression<T> child, Engine engine) {
+        super(child, OPERATOR_INFO, engine);
     }
 
     @Override
-    public int reductionOperation(int childResult) {
-        return -childResult;
-    }
-
-    @Override
-    public BigDecimal reductionOperation(BigDecimal childResult) {
-        return childResult.negate();
+    public T reductionOperation(T childResult) {
+        return engine.negate(childResult);
     }
 }
