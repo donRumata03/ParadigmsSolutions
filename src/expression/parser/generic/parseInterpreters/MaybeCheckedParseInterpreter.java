fae478dcdfea012201a87ab764e71e1c3ffbff3d
donRumata03 <donrumata03@gmail.com>
2022-03-22 16:29:10 +0300

    Template for MaybeCheckedParseInterpreter

--- src/expression/parser/generic/parseInterpreters/MaybeCheckedParseInterpreter.java
+++ src/expression/parser/generic/parseInterpreters/MaybeCheckedParseInterpreter.java
@@ -1,23 +1,94 @@
 package expression.parser.generic.parseInterpreters;
 
+import expression.Add;
+import expression.exceptions.CheckedAdd;
 import expression.general.ParenthesesTrackingExpression;
 import expression.parser.generic.tokens.AbstractOperationToken;
 
-public class MaybeCheckedParseInterpreter implements ParseInterpreter<Integer> {
+public class MaybeCheckedParseInterpreter extends TokenMatcher<Integer> {
+
+    private boolean checked = false;
+
+    public MaybeCheckedParseInterpreter(boolean checked) {
+        this.checked = checked;
+    }
 
     @Override
     public Integer parseSignedInt(String toParse) {
         return Integer.parseInt(toParse);
     }
 
-    @Override
-    public ParenthesesTrackingExpression<Integer> constructUnaryExpression(AbstractOperationToken token, ParenthesesTrackingExpression<Integer> child) {
 
+    @Override
+    ParenthesesTrackingExpression<Integer> constructLeadingZeroes(ParenthesesTrackingExpression<Integer> child) {
+        return null;
     }
 
     @Override
-    public ParenthesesTrackingExpression<Integer> constructBinaryExpression(
-        AbstractOperationToken token, ParenthesesTrackingExpression<Integer> left,
+    ParenthesesTrackingExpression<Integer> constructTrailingZeroes(ParenthesesTrackingExpression<Integer> child) {
+        return null;
+    }
+
+    @Override
+    ParenthesesTrackingExpression<Integer> constructAbs(ParenthesesTrackingExpression<Integer> child) {
+        return null;
+    }
+
+    @Override
+    ParenthesesTrackingExpression<Integer> constructUnaryMinus(ParenthesesTrackingExpression<Integer> child) {
+        return null;
+    }
+
+    @Override
+    ParenthesesTrackingExpression<Integer> constructAdd(ParenthesesTrackingExpression<Integer> left,
+        ParenthesesTrackingExpression<Integer> right) {
+        return checked ? new CheckedAdd(left, right) : new Add(left, right);
+    }
+
+    @Override
+    ParenthesesTrackingExpression<Integer> constructSubtract(ParenthesesTrackingExpression<Integer> left,
+        ParenthesesTrackingExpression<Integer> right) {
+        return null;
+    }
+
+    @Override
+    ParenthesesTrackingExpression<Integer> constructMultiply(ParenthesesTrackingExpression<Integer> left,
+        ParenthesesTrackingExpression<Integer> right) {
+        return null;
+    }
+
+    @Override
+    ParenthesesTrackingExpression<Integer> constructDivide(ParenthesesTrackingExpression<Integer> left,
+        ParenthesesTrackingExpression<Integer> right) {
+        return null;
+    }
+
+    @Override
+    ParenthesesTrackingExpression<Integer> constructShiftLeft(ParenthesesTrackingExpression<Integer> left,
+        ParenthesesTrackingExpression<Integer> right) {
+        return null;
+    }
+
+    @Override
+    ParenthesesTrackingExpression<Integer> constructLogicalShiftRight(ParenthesesTrackingExpression<Integer> left,
+        ParenthesesTrackingExpression<Integer> right) {
+        return null;
+    }
+
+    @Override
+    ParenthesesTrackingExpression<Integer> constructArithmeticShiftRight(ParenthesesTrackingExpression<Integer> left,
+        ParenthesesTrackingExpression<Integer> right) {
+        return null;
+    }
+
+    @Override
+    ParenthesesTrackingExpression<Integer> constructPow(ParenthesesTrackingExpression<Integer> left,
+        ParenthesesTrackingExpression<Integer> right) {
+        return null;
+    }
+
+    @Override
+    ParenthesesTrackingExpression<Integer> constructLog(ParenthesesTrackingExpression<Integer> left,
         ParenthesesTrackingExpression<Integer> right) {
         return null;
     }
