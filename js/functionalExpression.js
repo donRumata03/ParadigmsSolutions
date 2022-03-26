d54663ea6639bf6185d9cd775ef3ad64ecfac705
donRumata <donrumata03@gmail.com>
2022-03-26 19:47:35 +0300

    Solve hard mode of functionalExpression HT

--- js/functionalExpression.js
+++ js/functionalExpression.js
@@ -1,52 +1,63 @@
 "use strict"
 
-let cnst = value => (_x, _y, _z) => value
-let variable = name => (x, y, z) => name === "x" ? x : (name === "y" ? y : z)
+let cnst = value => (_x, _y, _z) => value;
+let variable = name => (x, y, z) => name === "x" ? x : (name === "y" ? y : z);
 
-let binaryOp = (op) => (l, r) => (x, y, z) => op(l(x, y, z), r(x, y, z))
+let binaryOp = (op) => (l, r) => (x, y, z) => op(l(x, y, z), r(x, y, z));
 
-let add = binaryOp((x, y) => x + y)
-let subtract = binaryOp((x, y) => x - y)
-let multiply = binaryOp((x, y) => x * y)
-let divide = binaryOp((x, y) => x / y)
+let add = binaryOp((x, y) => x + y);
+let subtract = binaryOp((x, y) => x - y);
+let multiply = binaryOp((x, y) => x * y);
+let divide = binaryOp((x, y) => x / y);
 
-let negate = (child) => (x, y, z) => -child(x, y, z)
+let negate = (child) => (x, y, z) => -child(x, y, z);
 
 let binaryOperators = {
     "+": add,
     "-": subtract,
     "*": multiply,
     "/": divide
-}
+};
 let unaryOperators = {
     "negate": negate
-}
+};
 
 let lexer = function (string) {
     let ptr = 0;
-    let scanWhile = predicate => {
-        let newPtr = ptr;
+    let scanWhile = predicate => start => {
+        let newPtr = start;
         while (newPtr < string.length && predicate(string[newPtr])) newPtr++;
         return newPtr;
+    }
+    let matchNextChars = function (...args) {
+        let tmpPtr = ptr;
+        for (const matcher of args) {
+            if (tmpPtr === string.length || !matcher(string[tmpPtr])) return false;
+            tmpPtr++;
+        }
+        return true;
     }
     let isDigit = ch => ch.match(/[0-9]/i);
     let isAlpha = ch => ch.toLowerCase().match(/[a-z]/i);
 
     return () => {
-        ptr = scanWhile(ch => ch.trim() === '')
+        ptr = scanWhile(ch => ch.trim() === '')(ptr)
         if (ptr === string.length) return undefined;
 
-        if (isAlpha(string[ptr])) {
-            return variable(string[ptr++]);
-        } else if (isDigit(string[ptr])) {
-            let afterDigitEnd = scanWhile(isDigit)
-            let res = string.substring(ptr, afterDigitEnd);
-            ptr = afterDigitEnd;
+        if (isDigit(string[ptr]) || matchNextChars(ch => isDigit(ch) || ch === '-', isDigit)) {
+            let afterNumberEnd = scanWhile(isDigit)(ptr + 1);
+            let res = string.substring(ptr, afterNumberEnd);
+            ptr = afterNumberEnd;
             return cnst(Number.parseInt(res));
-        } else {
-            if (!string[ptr] in binaryOperators) throw new Error()
+        } else if (string[ptr] in binaryOperators) {
             return binaryOperators[string[ptr++]];
-        }
+        } else if (isAlpha(string[ptr])) {
+            let afterWordEnd = scanWhile(isAlpha)(ptr);
+            let word = string.substring(ptr, afterWordEnd);
+            ptr = afterWordEnd;
+            if (word in unaryOperators) return unaryOperators[word];
+            return variable(word);
+        } else throw new Error();
     }
 }
 
@@ -59,7 +70,9 @@ let parse = function (string) {
         let next = lex();
         if (next === undefined) break;
 
-        if (Object.values(binaryOperators).includes(next)) {
+        if (Object.values(unaryOperators).includes(next)) {
+            stack.push(next(stack.pop()))
+        } else if (Object.values(binaryOperators).includes(next)) {
             let r = stack.pop()
             let l = stack.pop();
             stack.push(next(l, r));
@@ -73,7 +86,7 @@ let parse = function (string) {
 
 // console.log(parse("x x 2 - * x * 1 +")(5, 0, 0))
 // console.log(parse("100000")(0.00000000000000000000,0.00000000000000000000,0.00000000000000000000))
-
+console.log(parse("-314 negate -819 x - *")(5, 0, 0))
 //
 // let expr = subtract(
 //     multiply(
@@ -84,3 +97,4 @@ let parse = function (string) {
 // );
 //
 // console.log(expr(5, 0, 0));
+
