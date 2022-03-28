732f8289196967c65ee0c3ba6f364cfb9bd9f774
donRumata <donrumata03@gmail.com>
2022-03-28 21:50:08 +0300

    Remove old parser

--- java/expression/exceptions/TripleParser.java
+++ java/expression/exceptions/TripleParser.java
@@ -1,11 +1,12 @@
 package expression.exceptions;
 
 import expression.TripleExpression;
+import expression.parser.generic.ParseException;
 
 /**
  * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
  */
 @FunctionalInterface
 public interface TripleParser {
-    TripleExpression parse(String expression) throws Exception;
+    TripleExpression parse(String expression) throws ParseException;
 }
